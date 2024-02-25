7615bb4597a68063465efe77624207667d66d60f
adamd <adam.d.hinton@gmail.com>
2024-02-25 11:41:16 -0500

    type casting

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,6 @@ public class Main {
             Movie movie = Movie.getMovie(type, title);
             movie.watchMovie();
 
-
         }
 
 
---
+++ src/NextMain.java
@@ -0,0 +1,12 @@
+public class NextMain {
+
+    public static void main(String[] args) {
+
+        Movie movie = Movie.getMovie("A", "Jaws");
+        movie.watchMovie();
+
+        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+
+    }
+
+}
