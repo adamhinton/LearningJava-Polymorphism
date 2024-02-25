0667dd4b0b5c1683d89c78d4936afe25b96df326
adamd <adam.d.hinton@gmail.com>
2024-02-25 11:17:32 -0500

    polymorphic override method with three classes

--- src/Movie.java
+++ src/Movie.java
@@ -7,12 +7,59 @@ public class Movie {
     }
 
     public void watchMovie(){
-
         String instanceType = this.getClass().getSimpleName();
+        System.out.printf("%s is a %s film\n", title, instanceType);
+    }
+}
 
-        System.out.printf("title is a %s film", instanceType);
 
 
+class Adventure extends Movie{
+    public Adventure(String title) {
+        super(title);
     }
 
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Pleasant scene",
+                "Scary music",
+                "something bad happens");
+    }
 }
+
+
+
+class Comedy extends Movie{
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Something funny happens",
+                "Something even funnier happens",
+                "Happy ending");
+    }
+}
+
+
+
+class ScienceFiction extends Movie{
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Bad aliens do bad stuff",
+                "Space guys chase aliens",
+                "Planet blows up");
+    }
+}
+
