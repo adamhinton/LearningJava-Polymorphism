f7de2ac544d1a4c67088e5705c7b80f8d70e380b
adamd <adam.d.hinton@gmail.com>
2024-02-25 11:32:49 -0500

    scanner to determine polymorphed type

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Movie theMovie = new Adventure("Star Wars");
-        theMovie.watchMovie();
+//        Movie theMovie = Movie.getMovie("Science Fiction", "Star Wars");
+//        theMovie.watchMovie();
+
+
+        Scanner s = new Scanner(System.in);
+
+        while(true){
+            System.out.print("Enter type (A for Adventure, C for Comedy, S for Science Fiction, or Q to Quit): ");
+
+            String type = s.nextLine();
+
+            if("Qq".contains(type)){
+                break;
+            }
+
+            System.out.print("Enter movie title: ");
+
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+
+
+        }
+
 
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -6,6 +6,16 @@ public class Movie {
         this.title = title;
     }
 
+    public static Movie getMovie(String type, String title){
+
+        return switch(type.toUpperCase().charAt(0)){
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
+
     public void watchMovie(){
         String instanceType = this.getClass().getSimpleName();
         System.out.printf("%s is a %s film\n", title, instanceType);
